db442ca7349a5fa46a2b9d853e585e816115a60b
PJC-1 <jumpzine@gmail.com>
2018-01-03 21:06:36 -0800

    fixed mapStateToProps typo with curlies vs parens for return

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+  return [
+    { title: 'Javascript: The Good Parts' },
+    { title: 'Javascript & Jquery' },
+    { title: 'Cracking the Coding Interview' },
+    { title: 'Learn Ruby the Hard Way' }
+  ];
+}
